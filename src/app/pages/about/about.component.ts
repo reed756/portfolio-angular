d6bf9c64cf36f7587345b8348f335499f12b7043
reedanj <james.r@unitaskr.com>
2023-11-19 16:16:31 +0000

    make start on about page styles

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MaterialModule } from 'src/app/shared/material/material.module';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [MaterialModule, FontAwesomeModule]
 })
 export class AboutComponent {
 
